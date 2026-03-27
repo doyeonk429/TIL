180c49978e3e1189ccd605c74bb267a458118d96
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2026-03-27 17:31:57 +0900

    [MacOS App] add NSColorTransformer

---
+++ MacOS_App/Reminders/Reminders/Transformers/NSColorTransformer.swift
@@ -0,0 +1,34 @@
+//
+//  NSColorTransformer.swift
+//  Reminders
+//
+//  Created by 김도연 on 3/22/26.
+//
+
+import AppKit
+import Foundation
+
+class NSColorTransformer: ValueTransformer {
+    
+    override func transformedValue(_ value: Any?) -> Any? {
+        guard let color = value as? NSColor else { return nil }
+        
+        do {
+            let data = try NSKeyedArchiver.archivedData(withRootObject: color, requiringSecureCoding: true)
+            return data
+        } catch {
+            return nil
+        }
+    }
+    
+    override func reverseTransformedValue(_ value: Any?) -> Any? {
+        guard let data = value as? Data else { return nil }
+        
+        do {
+            let color = try NSKeyedUnarchiver.unarchivedObject(ofClass: NSColor.self, from: data)
+            return color
+        } catch {
+            return nil
+        }
+    }
+}
